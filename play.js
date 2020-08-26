0c080470875627a99b6d9c6e598af5118af44cea
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-08-26 18:15:48 +0000

    add stdin controls for user input

--- play.js
+++ play.js
@@ -2,3 +2,24 @@ const {connect} = require("./client");
 
 console.log('Connecting ...');
 connect();
+
+/*
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  const handleUserInput = function(data) {
+    if (data === '\u0003') {
+      process.exit();
+    }
+  };
+
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+setupInput();
